77d6448f92747c2b87a1c55b7c7e30290b6ceeb8
raphaelluizschettino <raphaelluiz128@hotmail.com>
2021-03-07 18:34:39 -0300

    initial structure angularjs

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodejsMysqlAngularjsFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nodejsMysqlAngularjsFrontApp
+ */
+angular.module('nodejsMysqlAngularjsFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
